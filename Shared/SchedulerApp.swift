8e4d416191e2d1b9100f132ff2a1bc2f178f90e6
alexanderschaller <alexjschaller@gmail.com>
2021-02-02 18:25:46 +0100

    Initial Commit

---
+++ Shared/SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulerApp.swift
+//  Shared
+//
+//  Created by Alexander Schaller on 02/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
